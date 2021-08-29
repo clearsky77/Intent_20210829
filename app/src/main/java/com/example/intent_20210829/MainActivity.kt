84d213a2fc317b4d03de8bd35e58053f40103368
clearsky77 <clearsky3264@gmail.com>
2021-08-29 15:15:48 +0900

    입력한 내용 담아두기 + 인텐트로 단순 화면 이동만.

--- app/src/main/java/com/example/intent_20210829/MainActivity.kt
+++ app/src/main/java/com/example/intent_20210829/MainActivity.kt
@@ -16,6 +16,18 @@ class MainActivity : AppCompatActivity() {
             val myIntent = Intent(this, OtherActivity2::class.java) // (출발지, 목적지)
             startActivity(myIntent) //이 변수보고 이동하자.
 
+
+//            메시지 보내기가 눌리면?
+            sendMessageBtn.setOnClickListener {
+//            입력한 내용이 어떻게 되는지 변수에 저장
+                var inputContent = messageEdt.text.toString()
+//            메시지 조회 화면으로 입력 내용을 가지고 이동
+                val myIntent = Intent(this, ViewMessageActivity::class.java)
+                startActivity(myIntent)
+
+            }
+
+
         }
     }
 }
