7324ef481b662edd571c214010df08ae74e54853
clearsky77 <clearsky3264@gmail.com>
2021-08-29 14:58:23 +0900

    메인 복귀시 화면 쌓이는 문제 발생.

--- app/src/main/java/com/example/intent_20210829/MainActivity.kt
+++ app/src/main/java/com/example/intent_20210829/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         moveToOtherBtn.setOnClickListener {
 //            다른 화면으로 이동하기가 눌리면
 //            어느 화면으로 갈 것인지 정보 명시.
-            val myIntent = Intent(this, OtherActivity2::class.java) //여기서 액티비티로 가겠다.
+            val myIntent = Intent(this, OtherActivity2::class.java) // (출발지, 목적지)
             startActivity(myIntent) //이 변수보고 이동하자.
 
         }
--- app/src/main/java/com/example/intent_20210829/OtherActivity2.kt
+++ app/src/main/java/com/example/intent_20210829/OtherActivity2.kt
@@ -1,11 +1,19 @@
 package com.example.intent_20210829
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other2.*
 
 class OtherActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other2)
+
+        returnToMainBtn.setOnClickListener {
+//            Intent를 이용 메인으로 이동
+
+
+        }
     }
 }
